f254e9e7ff97af87d06219409fd6fc9987537e1c
Marmo77 <matyskamela@gmail.com>
2025-08-19 04:32:27 +0200

    fixes #2
    Now filtering works perfectly fine, queries are pushed to array of queries based on whats inside the inputs

--- src/appwrite.ts
+++ src/appwrite.ts
@@ -41,37 +41,75 @@ export const show_all_cars = async () => {
     return [];
   }
 };
-export const getFilteredCars = async (brand: string) => {
-  try {
-    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
-      Query.equal("brand", brand.toLowerCase()),
-    ]);
-    console.log("Filtered cars: ", result.documents);
-    return result.documents;
-  } catch (error) {
-    console.error("Error fetching Filtered cars ❌: ", error);
-    return [];
-  }
-};
-export const getSearchedCars = async (search: string) => {
-  try {
-    const terms = search.split(" ");
+// export const getFilteredCars = async (brand: string) => {
+//   try {
+//     const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+//       Query.equal("brand", brand.toLowerCase()),
+//     ]);
+//     console.log("Filtered cars: ", result.documents);
+//     return result.documents;
+//   } catch (error) {
+//     console.error("Error fetching Filtered cars ❌: ", error);
+//     return [];
+//   }
+// };
+// export const getSearchedCars = async (search: string) => {
+//   try {
+//     const terms = search.split(" ");
 
-    const queries = terms.map((term) =>
-      Query.or([Query.contains("brand", term), Query.contains("model", term)])
-    );
+//     const queries = terms.map((term) =>
+//       Query.or([Query.contains("brand", term), Query.contains("model", term)])
+//     );
 
+//     const result = await database.listDocuments(
+//       DATABASE_ID,
+//       COLLECTION_ID,
+//       queries
+//     );
+//     //   Query.contains("brand", search) || Query.contains("model", search),
+//     //   Query.equal("brand", search) ||
+//     console.log("Searched cars: ", result.documents);
+//     return result.documents;
+//   } catch (error) {
+//     console.error("Error fetching Searched cars ❌: ", error);
+//     return [];
+//   }
+// };
+
+export const getFilteredAndSearchedCars = async (
+  brand: string,
+  search: string
+) => {
+  try {
+    // tworze array z queires zeby zbieral queires
+    const queries = [];
+
+    // sprawdzam czy brand(marka z comboboxa nie jest pusta (wybrane - Wszystkie, jesli tak to nie dodaje tych Queries do arraya))
+    if (brand && brand !== "all") {
+      queries.push(Query.equal("brand", brand.toLowerCase()));
+    }
+
+    // teraz sprawdzamy czy użytkownik wyszukiwał (jeśli wyszukiwanie istnieje i ma długość większą niż 2 znaki to wtedy bierze pod uwage (pushuje do naszych kwerend))
+
+    if (search && search.length > 1) {
+      const terms = search.split(" ");
+      terms.forEach((term) =>
+        queries.push(
+          Query.or([
+            Query.contains("brand", term),
+            Query.contains("model", term),
+          ])
+        )
+      );
+    }
     const result = await database.listDocuments(
       DATABASE_ID,
       COLLECTION_ID,
       queries
     );
-    //   Query.contains("brand", search) || Query.contains("model", search),
-    //   Query.equal("brand", search) ||
-    console.log("Searched cars: ", result.documents);
+    console.log("Filtered and Searched Cars: ", result);
     return result.documents;
   } catch (error) {
-    console.error("Error fetching Searched cars ❌: ", error);
-    return [];
+    console.error("Error in filtering and searching Cars! ", error);
   }
 };

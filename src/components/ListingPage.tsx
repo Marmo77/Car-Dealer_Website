2621d2a3fabcb9e1257e04b1e08df0336e483f4a
Marmo77 <matyskamela@gmail.com>
2025-09-03 17:39:46 +0200

    scrollback to top

--- src/components/ListingPage.tsx
+++ src/components/ListingPage.tsx
@@ -3,8 +3,26 @@ import BrowseMenu from "./ListingPage/BrowseMenu";
 import Filters from "./ListingPage/Filters";
 import AllCars from "./ListingPage/AllCars";
 import { useSearchParams } from "react-router-dom";
+import ScrollTopButton from "./ui/scrolltop";
 
 const ListingPage = () => {
+  //viewport scroll top button
+  const [showScrollTop, setShowScrollTop] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setShowScrollTop(true);
+      } else {
+        setShowScrollTop(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   // ########## DRIVING STATE TO URL
 
   const [searchParams, setSearchParams] = useSearchParams();
@@ -57,39 +75,42 @@ const ListingPage = () => {
   const [totalCars, setTotalCars] = useState<number>(0);
 
   return (
-    <section className="bg-card py-16 mx-auto w-full">
-      <div className="max-w-7xl px-12 sm:px-6 mx-auto grid grid-cols-4">
-        <div className="col-span-4">
-          <BrowseMenu
-            sortBy={sortBy}
-            setSortBy={setSortBy}
-            searchTerm={searchTerm}
-            setSearchTerm={setSearchTerm}
-            totalCars={totalCars}
-            viewMode={viewMode}
-            setViewMode={setViewMode}
-          />
+    <>
+      <section className="bg-card py-16 mx-auto w-full">
+        <div className="max-w-7xl px-12 sm:px-6 mx-auto grid grid-cols-4">
+          <div className="col-span-4">
+            <BrowseMenu
+              sortBy={sortBy}
+              setSortBy={setSortBy}
+              searchTerm={searchTerm}
+              setSearchTerm={setSearchTerm}
+              totalCars={totalCars}
+              viewMode={viewMode}
+              setViewMode={setViewMode}
+            />
+          </div>
+          <div className="col-span-1">
+            <Filters
+              filters={filters}
+              setFilters={setFilters}
+              clearFilters={clearFilters}
+            />
+          </div>
+          <div className={`col-span-3`}>
+            <AllCars
+              searchTerm={searchTerm}
+              sortBy={sortBy}
+              filters={filters}
+              isLoading={isLoading}
+              setIsLoading={setIsLoading}
+              setTotalCars={setTotalCars}
+              viewMode={viewMode}
+            />
+          </div>
         </div>
-        <div className="col-span-1">
-          <Filters
-            filters={filters}
-            setFilters={setFilters}
-            clearFilters={clearFilters}
-          />
-        </div>
-        <div className={`col-span-3`}>
-          <AllCars
-            searchTerm={searchTerm}
-            sortBy={sortBy}
-            filters={filters}
-            isLoading={isLoading}
-            setIsLoading={setIsLoading}
-            setTotalCars={setTotalCars}
-            viewMode={viewMode}
-          />
-        </div>
-      </div>
-    </section>
+      </section>
+      <ScrollTopButton showScrollTop={showScrollTop} />
+    </>
   );
 };
 
